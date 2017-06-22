00981605550130be70930fd2c1777b1d52827e0b
Marcos Gorll <marcos.gorll@careem.com>
2017-06-22 10:42:13 +0200

    Removing peak random

--- src/main/java/com/careem/kmsandmore/business/PeakService.java
+++ src/main/java/com/careem/kmsandmore/business/PeakService.java
@@ -1,14 +1,12 @@
 package com.careem.kmsandmore.business;
 
-import java.util.Random;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class PeakService {
 
 	public double peakMultiplier(long timeInMillis) {
-		return new Random().nextDouble() + 1.0;
+		return /*new Random().nextDouble() +*/ 1.0;
 	}
 	
 }
